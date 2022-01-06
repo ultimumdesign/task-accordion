8ce5013e1d94289634463a0e556390ae0a8def7d
Jan Arcalas <jan.arcalas@recro.com>
2022-01-06 14:17:50 -0700

    feat: add modal and notification

--- src/recro-task-accordion/index.js
+++ src/recro-task-accordion/index.js
@@ -5,12 +5,15 @@ import { createHttpEffect } from '@servicenow/ui-effect-http'
 import styles from './styles.scss'
 
 import '@servicenow/now-accordion'
+import '@servicenow/now-alert'
 import '@servicenow/now-button'
-import '@servicenow/now-dropdown'
 import '@servicenow/now-highlighted-value'
 import '@servicenow/now-icon'
 import '@servicenow/now-input'
+import '@servicenow/now-modal'
+import '@servicenow/now-rich-text'
 import '@servicenow/now-textarea'
+import '@servicenow/now-typeahead'
 
 function isObjectEqual (obj, source) {
   return Object.keys(source).every(key => key in obj && obj[key].value === source[key].value)
@@ -19,10 +22,10 @@ function isObjectEqual (obj, source) {
 function isObjectValid (obj) {
   if (obj.status.value !== 'Exception Approved') {
     const required = ['url']
-    return required.every(key => obj[key].value !== '')
+    return required.every(key => (obj[key].value !== '' && obj[key].value !== null))
   } else {
     const required = ['justification', 'exception_approval_date', 'exception_approvers']
-    return required.every(key => obj[key].value !== '')
+    return required.every(key => (obj[key].value !== '' && obj[key].value !== null))
   }
 }
 
@@ -56,6 +59,24 @@ const view = (state, { updateState, dispatch }) => {
     dispatch('SAVE_BUTTON_WATCHED')
   }
 
+  // function getToday () {
+  //   let today = new Date()
+  //   let dd = today.getDate()
+  //   let mm = today.getMonth() + 1 // January is 0!
+  //   const yyyy = today.getFullYear()
+
+  //   if (dd < 10) {
+  //     dd = '0' + dd
+  //   }
+
+  //   if (mm < 10) {
+  //     mm = '0' + mm
+  //   }
+
+  //   today = yyyy + '-' + mm + '-' + dd
+  //   return today
+  // }
+
   const debugBar = properties.debugMode
     ? (
       <div>
@@ -68,56 +89,70 @@ const view = (state, { updateState, dispatch }) => {
     const statusItems = properties.statusItems.items.map(item => {
       return (
         <option
-          key={item.id} value={item.id} selected={task.status.value === item.id ? 'selected' : false}
+          key={item.id} value={item.id} selected={task.status.value === item.id}
         >{item.label}
         </option>
       )
     })
+    const approverItems = properties.approverItems.items
     const justifySection = state.selectedTask.status && state.selectedTask.status.value === 'Exception Approved'
       ? (
         <div>
-          <now-textarea
-            className='recro-task-form' name='justification'
-            label={state.selectedTask.justification.label} value={task.justification.value}
-            required={state.selectedTask.status && state.selectedTask.status.value === 'Exception Approved'}
-            rows={properties.textAreaRows}
-          />
           <div className='recro-form-wrapper'>
             <input
               className='form-control' name='exception_approval_date'
-              required='required' type='date' aria-required='true' value={task.exception_approval_date.value}
+              type='date' required={state.selectedTask.status && state.selectedTask.status.value === 'Exception Approved'}
+              invalid={state.selectedTask.exception_approval_date && !(state.selectedTask.exception_approval_date.value)}
               on-input={(e) => { inputChanged('selectedTask.exception_approval_date.value', e.target.value, task) }}
             />
             <label for='exception_approval_date'>{task.exception_approval_date.label} <now-icon icon='asterisk-fill' size='sm' /></label>
           </div>
-          <div className='recro-form-wrapper'>
-            <input
-              className='form-control' required='required' type='text' aria-required='true' value={task.exception_approvers.value}
-              on-input={(e) => { inputChanged('selectedTask.exception_approvers.value', e.target.value, task) }}
-            />
-            <label for='exception_approvers'>{task.exception_approvers.label} <now-icon icon='asterisk-fill' size='sm' /></label>
-          </div>
+          <now-textarea
+            className='recro-task-form' name='justification'
+            label={state.selectedTask.justification.label} value={task.justification.value}
+            required={state.selectedTask.status && state.selectedTask.status.value === 'Exception Approved'}
+            invalid={state.selectedTask.justification && !(state.selectedTask.justification.value)}
+            disabled={!task.justification.canWrite}
+            rows={properties.textAreaRows}
+          />
+          <now-typeahead-multi
+            manage-selected-items items={approverItems}
+            selected-items={state.selectedTask.exception_approvers.value
+              ? state.selectedTask.exception_approvers.value.split(',')
+                  .map(function (sysID, index) { return { id: sysID, label: state.selectedTask.exception_approvers.displayValue.split(',')[index] } })
+              : []}
+            className='recro-task-form' name='exception_approvers'
+            label={task.exception_approvers.label} search='initial'
+            required={state.selectedTask.status && state.selectedTask.status.value === 'Exception Approved'}
+            invalid={state.selectedTask.exception_approvers && !(state.selectedTask.exception_approvers.value)}
+            disabled={!task.exception_approvers.canWrite}
+          ><now-button on-click={() => updateState({ isModalOpen: true })} slot='end' icon='magnifying-glass-plus-outline' size='sm' className='inputBtnIcon' tooltip-content='Search record' />
+          </now-typeahead-multi>
         </div>
         )
       : (
         <now-input-url
           className='recro-task-form'
-          value={task.url.value} label={task.url.label}
+          value={task.url.value} label={task.url.label} placeholder='https://'
           name='url' required={(state.selectedTask.status && state.selectedTask.status.value !== 'Exception Approved')}
         />
         )
     return (
       <now-accordion-item
-        on-click={(e) => taskItemClicked(task)} header={task.task_catalog_ref.displayValue}
-        key={task.sys_id.value} caption={{ label: task.description.value, variant: 'primary' }}
+        className='recro-accordion-item'
+        on-click={(e) => taskItemClicked(task)} header={{ label: task.task_catalog_ref.displayValue, variant: 'secondary', size: 'sm' }}
+        key={task.sys_id.value} caption={{ label: task.is_required.value ? 'Required' : 'Optional', variant: 'secondary', style: 'italic' }}
       >
         <now-highlighted-value
           slot='metadata' label={task.status.value} variant='tertiary'
         />
         <div slot='content'>
+          <div className='recro-task-description'>
+            <now-rich-text html={task.task_catalog_ref._reference ? task.task_catalog_ref._reference.task_description.value : '<h5>Test Description</h5>'} />
+          </div>
           <div className='recro-form-wrapper'>
             <select
-              className='form-control' name='status' value={task.status.value}
+              className='form-control' name='status' value={state.selectedTask.status ? state.selectedTask.status.value : task.status.value}
               on-change={(e) => inputChanged('selectedTask.status.value', e.target.value, task)}
             >
               {statusItems}
@@ -131,6 +166,7 @@ const view = (state, { updateState, dispatch }) => {
               label='Save' variant='primary' size='md' icon='' config-aria={{}}
               tooltip-content='' disabled={state.isTaskObjectEqual || (state.isTaskValid === false)}
             />
+            {state.isAlertOpen ? <now-alert status='positive' icon='exclamation-triangle-fill' header='Success:' content='Task was updated.' /> : <span />}
           </div>
         </div>
       </now-accordion-item>
@@ -141,11 +177,18 @@ const view = (state, { updateState, dispatch }) => {
       <now-accordion
         expand-single
         heading-level='2'
-        trigger-icon={{ type: 'chevron', position: 'end' }}
+        trigger-icon={{ type: 'chevron', position: 'start' }}
       >
         {taskItems}
         {debugBar}
       </now-accordion>
+      <now-modal
+        manage-opened
+        opened={state.isModalOpen}
+        size='lg'
+        header-label='Users'
+      ><now-record-list-connected hide-header hide-row-selector hide-quick-edit hide-inline-editing limit='10' table='sys_user' columns='name, email' />
+      </now-modal>
     </div>
   )
 }
@@ -166,6 +209,8 @@ createCustomElement('recro-task-accordion', {
     TASK_BUTTON_SAVE_SUCCESS: ({ action, dispatch, updateState, state, properties }) => {
       console.info('SAVE SUCCESS')
       dispatch('TASK_ACCORDION_REFRESH')
+      updateState({ isTaskObjectEqual: true })
+      updateState({ isAlertOpen: true })
     },
     TASK_ACCORDION_STATUS_REFRESH: ({ action, dispatch, updateProperties, state, properties }) => {
       updateProperties({ statusItems: { items: [...properties.statusItems.items] } })
@@ -210,23 +255,6 @@ createCustomElement('recro-task-accordion', {
         operation: 'set'
       })
     },
-    'NOW_INPUT_URL#INPUT': ({ action, dispatch, updateState, state, properties }) => {
-      const { name, data, fieldValue } = action.payload
-
-      if (properties.debugMode) {
-        console.debug('name:', name)
-        console.debug('data:', data)
-        console.debug('value:', fieldValue)
-      }
-
-      updateState({
-        path: `selectedTask.${name}.value`,
-        value: fieldValue,
-        operation: 'set'
-      })
-
-      dispatch('SAVE_BUTTON_WATCHED')
-    },
     'NOW_INPUT_URL#INVALID_SET': ({ action, dispatch, updateState, state, properties }) => {
       const { name, value, fieldValue } = action.payload
 
@@ -242,25 +270,101 @@ createCustomElement('recro-task-accordion', {
         operation: 'set'
       })
     },
-    'NOW_TEXTAREA#INPUT': ({ action, dispatch, updateState, state, properties }) => {
-      const { name, fieldValue } = action.payload
+    'NOW_INPUT_URL#VALUE_SET': ({ action, dispatch, updateState, state, properties }) => {
+      const { name, value } = action.payload
 
       if (properties.debugMode) {
         console.debug('name:', name)
-        console.debug('value:', fieldValue)
+        console.debug('value:', value)
       }
 
       updateState({
         path: `selectedTask.${name}.value`,
-        value: fieldValue,
+        value,
         operation: 'set'
       })
 
       dispatch('SAVE_BUTTON_WATCHED')
+    },
+    'NOW_TYPEAHEAD_MULTI#INVALID_SET': ({ action, dispatch, updateState, state, properties }) => {
+      const { name, value, fieldValue } = action.payload
+
+      if (properties.debugMode) {
+        console.debug('name:', name)
+        console.debug('invalid:', value)
+        console.debug('value:', fieldValue)
+      }
+
+      updateState({
+        path: 'isTaskValid',
+        value: false,
+        operation: 'set'
+      })
+    },
+    'NOW_TYPEAHEAD_MULTI#SELECTED_ITEMS_SET': ({ action, dispatch, updateState, state, properties }) => {
+      const { name, value } = action.payload
+
+      const newValue = value.reduce((prev, curr, index, list) => {
+        if (prev === '') return `${curr.id}`
+        else return `${prev},${curr.id}`
+      }, '')
+      const newDisplayValue = value.reduce((prev, curr, index) => {
+        if (prev === '') return `${curr.label}`
+        else return `${prev},${curr.label}`
+      }, '')
+      if (properties.debugMode) {
+        console.debug('name:', name)
+        console.debug('value:', value)
+        console.debug('newVal:', newValue)
+        console.debug('newDisplayvalue:', newDisplayValue)
+      }
+      updateState({
+        path: `selectedTask.${name}.value`,
+        value: newValue,
+        operation: 'set'
+      })
+
+      updateState({
+        path: `selectedTask.${name}.displayValue`,
+        value: newDisplayValue,
+        operation: 'set'
+      })
+
+      dispatch('SAVE_BUTTON_WATCHED')
+    },
+    'NOW_MODAL#OPENED_SET': ({ action, dispatch, updateState, state, properties }) => {
+      const { value } = action.payload
+
+      if (properties.debugMode) {
+        console.debug('value:', value)
+      }
+
+      updateState({
+        path: 'isModalOpen',
+        value: false,
+        operation: 'set'
+      })
+    },
+    'NOW_RECORD_LIST_CONNECTED#ROW_CLICKED': ({ action, dispatch, updateState, state, properties }) => {
+      const item = action.payload
+      const newApproverItem = { id: item.sys_id, label: item.row.displayValue.value }
+      if (properties.debugMode) {
+        console.debug('approver item:', newApproverItem)
+      }
+      const valueAsArray = state.selectedTask.exception_approvers.value.split(',')
+        .map(function (sysID, index) { return { id: sysID, label: state.selectedTask.exception_approvers.displayValue.split(',')[index] } })
+      dispatch('NOW_TYPEAHEAD_MULTI#SELECTED_ITEMS_SET', { name: 'exception_approvers', value: [...valueAsArray, newApproverItem] })
+      updateState({
+        path: 'isModalOpen',
+        value: false,
+        operation: 'set'
+      })
     }
   },
   initialState: {
     selectedTask: {},
+    isAlertOpen: false,
+    isModalOpen: false,
     isTaskObjectEqual: true,
     isTaskValid: false
   },
@@ -292,6 +396,11 @@ createCustomElement('recro-task-accordion', {
         // { id: 'completed', label: 'Completed' },
         // { id: 'delayed'', label: 'Delayed' },
         // { id: 'exception_approved', label: 'Exception Approved' }
+      ]
+    },
+    approverItems: {
+      default: [
+        // { id: 'sys_id', label: 'John Doe' },
       ]
     },
     debugMode: {
